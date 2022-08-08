b87a1bbaf5891e3e40bb6abb3bb7e114e890d6b6
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-08 22:10:22 +0300

    Task_4

--- Program.cs
+++ Program.cs
@@ -9,4 +9,24 @@ if (a>b)
   else
   Console.WriteLine($"{0} is max number {b}");
 */
-
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Input first number:");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number:");
+int w = Convert.ToInt32(Console.ReadLine());
+if (y>x)
+ Console.WriteLine($"{y} is max number:");
+ else
+ Console.WriteLine($"{x} is max number:");
+ if (w>y)
+ Console.WriteLine($"{w} is max number:");
+else
+ Console.WriteLine($"{y} is max number:");
+ if (w>x)
+  Console.WriteLine($"{w} is max number:");
+  else
+   Console.WriteLine($"{x} is max number:");
+   */
